262ab441e410515ab82ca876fae070a94ea192b0
wogks <wogks27@naver.com>
2024-04-27 16:54:17 +0900

    Feat: bottomnavibar

--- lib/common/layout/default_layout.dart
+++ lib/common/layout/default_layout.dart
@@ -3,13 +3,34 @@ import 'package:flutter/material.dart';
 class DefaultLayout extends StatelessWidget {
   final Widget child;
   final Color? color;
-  const DefaultLayout({super.key, required this.child, this.color});
+  final String? title;
+  final Widget? bottomNavigationBar;
+  const DefaultLayout(
+      {super.key,
+      required this.child,
+      this.color,
+      this.title,
+      this.bottomNavigationBar});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: _renderAppbar(),
       backgroundColor: color ?? Colors.white,
       body: child,
+      bottomNavigationBar: bottomNavigationBar,
     );
+  }
+
+  AppBar? _renderAppbar() {
+    if (title == null) {
+      return null;
+    } else {
+      return AppBar(
+        backgroundColor: Colors.white,
+        elevation: 0,
+        title: Text(title!),
+      );
+    }
   }
 }
--- lib/common/view/root_tab.dart
+++ lib/common/view/root_tab.dart
@@ -1,13 +1,50 @@
+import 'package:codefacprac/common/const/colors.dart';
 import 'package:codefacprac/common/layout/default_layout.dart';
 import 'package:flutter/material.dart';
 
-class RootTab extends StatelessWidget {
+class RootTab extends StatefulWidget {
   const RootTab({super.key});
 
   @override
+  State<RootTab> createState() => _RootTabState();
+}
+
+class _RootTabState extends State<RootTab> {
+  int cuindex = 0;
+  @override
   Widget build(BuildContext context) {
-    return const DefaultLayout(
-      child: Center(
+    return DefaultLayout(
+      title: '배달앱',
+      bottomNavigationBar: BottomNavigationBar(
+        selectedItemColor: PRIMARY_COLOR,
+        unselectedItemColor: BODY_TEXT_COLOR,
+        type: BottomNavigationBarType.fixed,
+        currentIndex: cuindex,
+        onTap: (value) {
+          setState(() {
+            cuindex = value;
+          });
+        },
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home_outlined),
+            label: '홈',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.fastfood_outlined),
+            label: '음식',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.receipt_long_outlined),
+            label: '주문',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.person_outline_outlined),
+            label: '프로필',
+          ),
+        ],
+      ),
+      child: const Center(
         child: Text('root'),
       ),
     );
